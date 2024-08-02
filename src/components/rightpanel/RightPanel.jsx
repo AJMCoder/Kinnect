2ff854f7f01d0bf6bacc35a1551bddc8598c335e
Alex Mullins <alexmullins@hotmail.co.uk>
2024-08-02 21:56:26 +0000

    Add rightpanel details and begin styling.

--- src/components/rightpanel/RightPanel.jsx
+++ src/components/rightpanel/RightPanel.jsx
@@ -2,9 +2,21 @@ import "./rightPanel.css";
 
 const RightPanel = () => {
     return (
-        <div>
-            <div className="RightPanel">
-                RightPanel
+        <div className="RightPanel">
+            <div className="container">
+                <div className="sidebar">
+                    <span>Recommended</span>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <span>John Smith</span>
+                        </div>
+                        <div className="buttons">
+                            <button>Add</button>
+                            <button>Ignore</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
