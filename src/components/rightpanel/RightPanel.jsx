033414d991c1d1f8c5ce6e4c544de258a2487bd8
Alex Mullins <alexmullins@hotmail.co.uk>
2024-08-03 19:13:18 +0000

    Add 'Recent Activity' tab and 'Friends Online' tab for the right panel and style.

--- src/components/rightpanel/RightPanel.jsx
+++ src/components/rightpanel/RightPanel.jsx
@@ -17,6 +17,76 @@ const RightPanel = () => {
                         </div>
                     </div>
                 </div>
+                <div className="sidebar">
+                    <span>Recent Activity</span>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <p>
+                                <span>John Smith</span> posted a status update.
+                            </p>
+                        </div>
+                        <span>5 minutes ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <p>
+                                <span>John Smith</span> liked a comment.
+                            </p>
+                        </div>
+                        <span>5 minutes ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <p>
+                                <span>John Smith</span> liked a picture.
+                            </p>
+                        </div>
+                        <span>5 minutes ago</span>
+                    </div>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <p>
+                                <span>John Smith</span> posted a video.
+                            </p>
+                        </div>
+                        <span>5 minutes ago</span>
+                    </div>
+                </div>
+                <div className="sidebar">
+                    <span>Friends Online</span>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <div className="online" />
+                            <span>John Smith</span>
+                        </div>    
+                    </div>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <div className="online" />
+                            <span>John Smith</span>
+                        </div>    
+                    </div>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <div className="online" />
+                            <span>John Smith</span>
+                        </div>    
+                    </div>
+                    <div className="user">
+                        <div className="userDetails">
+                            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="" />
+                            <div className="online" />
+                            <span>John Smith</span>
+                        </div>    
+                    </div>
+                </div>
             </div>
         </div>
     );
